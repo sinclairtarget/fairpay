f9a4da64e6c1ad55323d98b646e835e72642bc8e
Sinclair Target <sinclairtarget@gmail.com>
2016-01-23 13:59:59 -0500

    User auth check.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :authorize
+
+  protected
+  def authorize
+    unless @user = User.find_by_id(session[:user_id])
+      @user = User.find(1)
+    end
+  end
 end
