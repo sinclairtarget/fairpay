591fd728628aded0e1b0c0fe60c2cc9093584852
Sinclair Target <sinclairtarget@gmail.com>
2016-02-21 17:55:43 -0500

    Add old unicorn process killing to unicorn.rb.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,8 +7,13 @@ working_directory app_dir
 
 # set unicorn options
 worker_processes 2
-preload_app true
 timeout 30
+
+# load application into master before fork for speediness
+# if the application opens any sockets or files those will be shared by the
+# worker children. Those sockets/files should be closed and reopened in the
+# worker children. SEE BELOW
+preload_app true 
 
 # set up socket location
 listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
@@ -19,3 +24,31 @@ stdout_path "#{shared_dir}/log/unicorn.stdout.log"
 
 # set location for pid file
 pid "#{shared_dir}/pids/unicorn.pid"
+
+before_fork do |server, worker|
+  # make sure the master process never holds a database socket connection
+  # it never needs one -- we don't want to mess with child process connections
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+  end
+
+  # when unicorn is restarted using the USR2 signal, it starts up a new master
+  # process but continues serving requests. The pid for the old master process
+  # is unicorn.pid.oldbin. We need to kill the old process if it exists
+  # https://unicorn.bogomips.org/SIGNALS.html
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
+
+after_fork do |server, worker|
+  # restablish the database connection we killed above
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+  end
+end
