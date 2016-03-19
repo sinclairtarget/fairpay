c1001dc46674a546a682090fb02f09ce00641b82
Sinclair Target <sinclairtarget@gmail.com>
2016-03-19 12:37:55 -0400

    Add helper for showing salary ranges.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
+require_relative "../../lib/util/util.rb"
+
 module ApplicationHelper
+  MIN_GROUP_MEMBERS = 3
+
+  def fudged_salary(salary, members)
+    return number_to_currency(salary.annual_pay) if members > 3
+
+    min, max = Util.fudge(salary.annual_pay, members)
+    number_to_currency(min) + " – " + number_to_currency(max)
+  end
 end
