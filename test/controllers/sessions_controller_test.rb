7915794e43ec2049a28cbdc91d5ff4151bce7377
Sinclair Target <sinclairtarget@gmail.com>
2016-05-15 18:08:12 -0400

    Login page now redirects to group index if user is already logged in.

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,18 +1,35 @@
 class SessionsControllerTest < ActionController::TestCase
+  setup do
+    user = User.find_by(email: "anne@test.com")
+    @session = { user_id: user.id }
+
+    @login_data = {
+      email: "anne@test.com",
+      password: "p@sswrd"
+    }
+  end
+
   test "can get login" do
     get :new
     assert_response :success
   end
 
+  test "if already logged in, redirect to groups index" do
+    get :new, nil, @session
+
+    assert_response :redirect
+    assert_redirected_to groups_path
+  end
+
   test "can login" do
-    post :create, { email: "anne@test.com", password: "p@sswrd" }
+    post :create, @login_data
 
     assert_response :redirect
     assert_redirected_to groups_path
   end
 
   test "can fail login" do
-    post :create, { email: "foo@test.com", password: "p@sswrd" }
+    post :create, @login_data.merge(email: "foo@test.com")
 
     assert_response :redirect
     assert_redirected_to login_path
