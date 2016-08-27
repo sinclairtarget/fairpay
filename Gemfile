2bf4b803a893187047a1d3fe0332c042aa1844a6
Sinclair Target <sinclairtarget@gmail.com>
2016-08-27 13:25:44 -0400

    No more bootstrap.

--- Gemfile
+++ Gemfile
@@ -34,8 +34,6 @@ gem 'nprogress-rails'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 
-gem 'bootstrap-sass'
-
 # Easy renaming of application
 gem 'rename'
 
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery.turbolinks
 //= require jquery_ujs
-//= require bootstrap-sprockets
 //= require nprogress
 //= require nprogress-turbolinks
 //= require turbolinks
